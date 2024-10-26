9c423d12959a5229a9f1ff228a2c93cc9ea1da22
EC2 Default User <ec2-user@ip-172-31-32-90.ap-northeast-1.compute.internal>
2024-10-26 08:34:06 +0000

    [add] books_days_date

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @total_views = @user.books.sum(&:impressionist_count)
     @book = Book.new
+    
+  today = Date.today
+  yesterday = today - 1
+  start_of_week = today.beginning_of_week
+  start_of_last_week = start_of_week - 1.week
+    
+  @today_books_count = @user.books.where(created_at: today.all_day).count
+  @yesterday_books_count = @user.books.where(created_at: yesterday.all_day).count
+  @weekly_books_count = @user.books.where(created_at: start_of_week..today).count
+  @last_week_books_count = @user.books.where(created_at: start_of_last_week..start_of_week - 1.day).count
+
+  @difference_today_yesterday = @today_books_count - @yesterday_books_count
+  @difference_weekly = @weekly_books_count - @last_week_books_count
+  
   end
+  
 
   def index
     @users = User.all
